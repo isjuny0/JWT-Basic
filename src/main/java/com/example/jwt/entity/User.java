ead18a67d3f200f0475b79a17faf713f480b22c5
GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>
2025-09-16 15:30:26 +0900

    feat: add User Entity

---
+++ src/main/java/com/example/jwt/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.jwt.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
